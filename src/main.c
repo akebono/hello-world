4a1cac5577aad7befe2f43304371f64c34bb9cff
Akebono <Akebono@list.ru>
2011-08-12 18:56:38 +0400

    sockets incoming

--- src/main.c
+++ src/main.c
@@ -28,9 +28,32 @@ void button_handle(GtkWidget *widget, gpointer data)
 	return FALSE;
 }
 
+void button1_handle(GtkWidget *widget, gpointer data)
+{
+	int s,i;
+	struct sockaddr sa;
+
+	s=socket(2,1,6);
+	sa.sa_family=2;
+	sa.sa_data[0]=0x10;
+	sa.sa_data[0]=0x20;
+	sa.sa_data[0]=127;
+	sa.sa_data[0]=0;
+	sa.sa_data[0]=0;
+	sa.sa_data[0]=1;
+	if(bind(s,&sa,16))
+		if(errno==98){
+			i=1;
+			setsockopt(s,SOL_SOCKET,SO_REUSE_ADDR,&i,1)
+		}
+		
+
+	return FALSE;
+}
+
 int main(int argc, char *argv[])
 {
-	GtkWidget *window, *label, *button, *fixed;
+	GtkWidget *window, *label, *button, *fixed, *button1;
 	buffer=malloc(64);
 	sprintf(buffer,"-");
 	gtk_init(&argc,&argv);
@@ -40,11 +63,13 @@ int main(int argc, char *argv[])
 	g_signal_connect(window,"destroy",G_CALLBACK(destroy),0);
 	label=gtk_label_new(0);
 	fixed=gtk_fixed_new();
-	button=gtk_button_new_with_label("start");
+	button=gtk_button_new_with_label("alsa");
+	button=gtk_button_new_with_label("socket");
 	
 	g_signal_connect(label,"expose-event",G_CALLBACK(expose_event),0); //this called immideatly with buffer filling
 	g_timeout_add(1000,(GSourceFunc)timer_handle,(gpointer)window);
-	g_signal_connect(button,"clicked",G_CALLBACK(button_handle),0); //this called immideatly with buffer filling
+	g_signal_connect(button,"clicked",G_CALLBACK(button_handle),0);
+	g_signal_connect(button,"clicked",G_CALLBACK(button1_handle),0);
 
 	gtk_fixed_put(GTK_FIXED(fixed),button,20,70);
 	gtk_fixed_put(GTK_FIXED(fixed),label,20,20);
